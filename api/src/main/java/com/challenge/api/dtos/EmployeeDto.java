0dceee83a131b1709e212bf9b3d5a01a359c0844
Eric Fidalgo <ericfidalgo.cs@gmail.com>
2025-10-03 10:22:43 -0400

    added getter and setter methods to employeeDto and BasicEmployee to clear the redundant code

--- api/src/main/java/com/challenge/api/dtos/EmployeeDto.java
+++ api/src/main/java/com/challenge/api/dtos/EmployeeDto.java
@@ -2,7 +2,12 @@ package com.employee.api.dtos;
 
 import java.util.UUID;
 
-// The Dtos main job is to send the employee information to the clients and to expose only what is needed for the user to see 
+import lombok.Getter;
+import lombok.Setter;
+
+// The Dtos main job is to send the employee information to the clients and to expose only what is needed for the user to see
+@Getter
+@Setter
 public class EmployeeDto {
 
     private UUID uuid;
@@ -11,52 +16,4 @@ public class EmployeeDto {
     private String fullName;
     private String jobTitle;
     private String email;
-
-    public UUID getUuid() {
-        return uuid;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public String getJobTitle() {
-        return jobTitle;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setUuid(UUID uuid) {
-        this.uuid = uuid;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public void setJobTitle(String jobTitle) {
-        this.jobTitle = jobTitle;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
--- api/src/main/java/com/challenge/api/model/BasicEmployee.java
+++ api/src/main/java/com/challenge/api/model/BasicEmployee.java
@@ -1,14 +1,21 @@
 package com.employee.api.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import java.time.Instant;
 import java.util.UUID;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 // This is the concrete implementation of the Employee interface which allows for loose coupling because we can implement different function methods with other classes 
-@Entity
+@Entity // JPA Entity 
 @Table(name = "employee")
+@Getter
+@Setter
+@NoArgsConstructor
 public class BasicEmployee implements Employee {
 
     @Id
@@ -23,104 +30,4 @@ public class BasicEmployee implements Employee {
     private String email;
     private Instant contractHireDate;
     private Instant contractTerminationDate;
-
-    @Override
-    public UUID getUuid() {
-        return uuid;
-    }
-
-    @Override
-    public void setUuid(UUID uuid) {
-        this.uuid = uuid;
-    }
-
-    @Override
-    public String getFirstName() {
-        return firstName;
-    }
-
-    @Override
-    public void setFirstName(String name) {
-        this.firstName = name;
-    }
-
-    @Override
-    public String getLastName() {
-        return lastName;
-    }
-
-    @Override
-    public void setLastName(String name) {
-        this.lastName = name;
-    }
-
-    @Override
-    public String getFullName() {
-        return fullName;
-    }
-
-    @Override
-    public void setFullName(String name) {
-        this.fullName = name;
-    }
-
-    @Override
-    public Integer getSalary() {
-        return salary;
-    }
-
-    @Override
-    public void setSalary(Integer salary) {
-        this.salary = salary;
-    }
-
-    @Override
-    public Integer getAge() {
-        return age;
-    }
-
-    @Override
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    @Override
-    public String getJobTitle() {
-        return jobTitle;
-    }
-
-    @Override
-    public void setJobTitle(String jobTitle) {
-        this.jobTitle = jobTitle;
-    }
-
-    @Override
-    public String getEmail() {
-        return email;
-    }
-
-    @Override
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public Instant getContractHireDate() {
-        return contractHireDate;
-    }
-
-    @Override
-    public void setContractHireDate(Instant date) {
-        this.contractHireDate = date;
-    }
-
-    @Override
-    public Instant getContractTerminationDate() {
-        return contractTerminationDate;
-    }
-
-    @Override
-    public void setContractTerminationDate(Instant date) {
-        this.contractTerminationDate = date;
-    }
 }
